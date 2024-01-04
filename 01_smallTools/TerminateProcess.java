7fd6d46e06a3061c49150a22a18431d9ca511211
aweill <84896083+aweill@users.noreply.github.com>
2024-01-04 20:50:51 +0800

    Update TerminateProcess.java
    1. 新增提示，如果进程不存在会给出提示，而不是显示进程已经终止
    2. 不依赖于 tasklist 或 ps 命令的输出缓存，通过使用 BufferedReader 来读取命令的输出流，确保每次都获取最新的信息

--- 01_smallTools/TerminateProcess.java
+++ 01_smallTools/TerminateProcess.java
@@ -1,20 +1,62 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TerminateProcess {
     public static void main(String[] args) {
         // 替换为要终止的进程的ID
-        int processIdToTerminate = 23420;
+        int processIdToTerminate = 20820;
 
         try {
-            // 终止进程
-            terminateProcess(processIdToTerminate);
+            // 检查进程是否存在
+            if (isProcessRunning(processIdToTerminate)) {
+                // 终止进程
+                terminateProcess(processIdToTerminate);
 
-            // 可以添加一些日志或其他处理来指示进程已经被终止
-            System.out.println("Process terminated gracefully.");
+                // 可以添加一些日志或其他处理来指示进程已经被终止
+                System.out.println("Process terminated gracefully.");
+            } else {
+                System.out.println("Process with ID " + processIdToTerminate + " does not exist.");
+            }
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean isProcessRunning(int processId) throws IOException, InterruptedException {
+        // 构建操作系统命令
+        String os = System.getProperty("os.name").toLowerCase();
+
+        String command;
+        if (os.contains("win")) {
+            // Windows
+            command = "tasklist /FI \"PID eq " + processId + "\"";
+        } else {
+            // Unix/Linux/Mac
+            command = "ps -p " + processId;
+        }
+
+        // 执行命令
+        Process process = Runtime.getRuntime().exec(command);
+
+        // 等待命令执行完毕
+        process.waitFor();
+
+        // 读取命令的输出流
+        InputStream inputStream = process.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        // 检查命令的输出来判断进程是否存在
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.contains(Integer.toString(processId))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private static void terminateProcess(int processId) throws IOException, InterruptedException {
